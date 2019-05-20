c2194b93ad75cb469c6aafff3d56433651ab1438
kyuridata <kyuripark1206@gmail.com>
2019-05-20 12:33:24 +0900

    reset commit

---
+++ wordcount/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')  #render : 세개의 인자까지 받을 수 있음
+
+def about(request):
+    return render(request, 'about.html')
+    
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split()
+    word_dictionary ={}
+
+    for word in words: 
+        if word in word_dictionary:
+            #increase
+            word_dictionary[word]+=1
+
+        else:
+            # add to dic
+            word_dictionary[word]=1
+
+
+    return render(request, 'result.html', {'full':text, 'total':len(words), 'dictionary' : word_dictionary.items()})
+    
+       
